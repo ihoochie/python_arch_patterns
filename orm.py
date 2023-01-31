9f34f30714509009e2e08da477f07c26113d18b5
Ilya Andrutskiy <i.andrutskiy@tinkoff.ru>
2023-01-31 23:30:59 +0400

    add orm with mappers

---
+++ orm.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy.orm import registry
+
+import model
+
+mapper_registry = registry()
+
+order_lines = Table(
+    'order_lines',
+    mapper_registry.metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('sku', String(255)),
+    Column('qty', Integer, nullable=False),
+    Column('orderid', String(255)),
+)
+
+
+def start_mappers():
+    mapper_registry.map_imperatively(model.OrderLine, order_lines)
