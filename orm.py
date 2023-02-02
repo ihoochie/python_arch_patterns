5fa148086601718f8d667ac2a62c053f15f52fe1
Ilya Andrutskiy <i.andrutskiy@tinkoff.ru>
2023-02-02 17:20:53 +0400

    repository with tests

--- orm.py
+++ orm.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Table, Column, Integer, String
-from sqlalchemy.orm import registry
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, Date
+from sqlalchemy.orm import registry, relationship
 
 import model
 
@@ -14,6 +14,33 @@ order_lines = Table(
     Column('orderid', String(255)),
 )
 
+batches = Table(
+    "batches",
+    mapper_registry.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("reference", String(255)),
+    Column("sku", String(255)),
+    Column("_purchased_quantity", Integer, nullable=False),
+    Column("eta", Date, nullable=True),
+)
+
+allocations = Table(
+    "allocations",
+    mapper_registry.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("orderline_id", ForeignKey("order_lines.id")),
+    Column("batch_id", ForeignKey("batches.id")),
+)
+
 
 def start_mappers():
-    mapper_registry.map_imperatively(model.OrderLine, order_lines)
+    lines_mapper = mapper_registry.map_imperatively(model.OrderLine, order_lines)
+    mapper_registry.map_imperatively(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                lines_mapper, secondary=allocations, collection_class=set
+            )
+        }
+    )
---
+++ repository.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+import model
+
+
+class AbstractRepository(ABC):
+    @abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get(self, reference) -> model.Batch:
+        raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(model.Batch).all()
