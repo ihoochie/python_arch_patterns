8bcd29418be2390a37cc3c11f3de84e0645f5355
Ilya Andrutskiy <i.andrutskiy@tinkoff.ru>
2023-02-15 00:55:22 +0400

    chapter code

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from src.allocation import config
 from src.allocation.domain import model
 from src.allocation.adapters import orm, repository
-from src.allocation.service_layer import services
+from src.allocation.service_layer import services, unit_of_work
 
 orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
@@ -15,18 +15,13 @@ get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
 
-def is_valid_sku(sku, batches):
-    return sku in {b.sku for b in batches}
-
-
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
 
     try:
         batchref = services.allocate(
-            request.json["orderid"], request.json["sku"], request.json["qty"], repo, session
+            request.json["orderid"], request.json["sku"], request.json["qty"], uow
         )
     except (model.OutOfStock, services.InvalidSku) as e:
         return jsonify({"message": str(e)}), 400
@@ -36,14 +31,13 @@ def allocate_endpoint():
 
 @app.route("/add_batch", methods=["POST"])
 def add_batch():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
     eta = request.json.get("eta")
 
     if eta is not None:
         eta = date.fromisoformat(eta)
 
     services.add_batch(
-        request.json["ref"], request.json["sku"], request.json["qty"], eta, repo, session
+        request.json["ref"], request.json["sku"], request.json["qty"], eta, uow
     )
     return "OK", 201
